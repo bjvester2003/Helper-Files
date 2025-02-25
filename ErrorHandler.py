06204a14994ff8debbf4d1ba617ec8c8df66cd5d
bjves <bjvester@gmail.com>
2025-02-25 15:30:06 -0500

    Excluded test file. Modified ErrorHandler.py

--- ErrorHandler.py
+++ ErrorHandler.py
@@ -1,5 +1,14 @@
+## KNOWN ERRORS
+# 1) If the log file is deleted but the containing directory remains an error will be caused.
+
+## THINGS TO ADD
+# 1) Implement Entry IDs. This should be a unique ID for each entry into the log file.
+# 2) Implement a function that allows for reading a specific entry ID
+# 3) Allow display of error upon writing.
+# 4) Rework to import and write data only once.
+
 import datetime as dt
-from csv import writer, reader
+from csv import writer, DictReader
 import os
 
 class ErrorHandler():
@@ -20,6 +29,7 @@ class ErrorHandler():
                 writer_object.writerow(self.error_entry)
                 write_file.close()
         else:
+            os.mkdir('/'.join(self.LogFile.split('/')[:-1]))
             with open(self.LogFile, 'w', newline='') as write_file:
                 writer_object = writer(write_file)
                 writer_object.writerow(["ERROR_CODE","ERROR_MESSAGE","TIME_ERROR"])
@@ -31,7 +41,14 @@ class ErrorHandler():
         error_history = []
         
         with open(self.LogFile, 'r') as read_file:
-            reader_object = reader(read_file)
+            reader_object = DictReader(read_file)
             for row in reader_object:
                 error_history.append(row)
             read_file.close()
+        
+        print(" <><><> ERROR HSTORY <><><>") 
+        for entry in error_history:            
+            print(f"Error Code: {entry["ERROR_CODE"]}")
+            print(f"Error Code: {entry["ERROR_MESSAGE"]}")
+            print(f"Error Code: {entry["TIME_ERROR"]}")
+            print("")
