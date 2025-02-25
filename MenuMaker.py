5eab06e516ee1725cf98654549bfb5e2c88d8f2f
bjves <bjvester@gmail.com>
2025-02-25 16:33:31 -0500

    Reworking this ish... nerd looking ahh...

--- MenuMaker.py
+++ MenuMaker.py
@@ -2,52 +2,44 @@ import datetime as dt
 from os import system, name
 
 class MenuMaker():
-    def __init__(self, menu_options, logo_file=None, enable_timestamp=False):
+    def __init__(self, menu_options:dict | list, logo_file:str=None, enable_timestamp:bool=False, submenu:bool=False):
         self.menu_options = menu_options
-        self.logo = self.import_logo(logo_file)
-        self.timestamp = self.build_timestamp(enable_timestamp)
+        self.logo = (self.import_logo(logo_file), None)[(logo_file != None) and (logo_file.split('.')[-1] == 'txt')]
+        self.timestamp = (self.build_timestamp(), None)[enable_timestamp]
+        self.submenu = submenu
     
     #### Initialization Methods ###    
     def import_logo(self, logo_file)->str:
-        """If file ends in .txt, imports the logo."""
-        if (logo_file != None) and (logo_file.split('.')[-1] == 'txt'):
-            with open(logo_file, "r") as file:
-                logo = file.read()
-                return logo
-        else:
-            return None
+        with open(logo_file, "r") as file:
+            logo = file.read()
+            return logo
 
-    def build_timestamp(self, enable_timestamp:bool)->str:
-        if enable_timestamp:
-            current_datetime = dt.datetime.now()
-            return(current_datetime.strftime(" %A, %B %d - %H:%M:%S"))
-        else:
-            return None 
+    def build_timestamp(self)->str:
+        current_datetime = dt.datetime.now()
+        return(current_datetime.strftime(" %A, %B %d - %H:%M:%S"))
         
     #### Menu Display Methods ###
     def clear_screen(self):
-         # for windows
-        if name == 'nt':
+        if name == 'nt': # for windows
             return system('cls')
-            
-        # for mac and linux(here, os.name is 'posix')
-        else:
+        elif name == 'posfix': # for mac and linux
             return system('clear')
+        
+    def display_logo(self):
+        print(self.logo)
+        
+    def display_timestamp(self):
+        print(self.timestamp)
         
     def display_menu_options(self):
         menu_divider = " "+"".join(["_" for _ in range(71)])+"\n"
         
-        # Add Extra Information # 
-        if self.logo != None:
-            print(self.logo)
-        if self.timestamp != None:
-            print(self.timestamp)    
-        
         print(menu_divider)
-        for num, op in enumerate(self.menu_options):
-            print(f" [{num+1}] {op}")
+        for option_number, option in enumerate(self.menu_options):
+            print(f" [{option_number+1}] {option}")
         print(menu_divider)
         
+    #### Selection Functions ####
     def get_menu_selection(self):
         valid_selections = [str(num+1) for num in range(0, len(self.menu_options))]
         menu_selection = input(" Menu Selection : ")
@@ -59,7 +51,8 @@ class MenuMaker():
             self.build_menu()
         else:
             return self.menu_options[int(menu_selection)-1]
-            
+    
+    #### Run Function ####        
     def build_menu(self):
         self.display_menu_options()
         return self.get_menu_selection()
